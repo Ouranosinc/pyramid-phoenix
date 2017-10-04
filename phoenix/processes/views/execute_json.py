5da5bee9ce8d7b87adf811ae7f119c74f33a541a
David Byrns <david.byrns@crim.ca>
2017-10-04 15:49:02 -0400

    Fix bad return type (mixed json and html content) and better exception description

--- phoenix/processes/views/execute_json.py
+++ phoenix/processes/views/execute_json.py
@@ -1,14 +1,12 @@
 import types
 from pyramid.view import view_config, view_defaults
-from pyramid.httpexceptions import HTTPBadRequest
 
 from owslib.wps import ComplexData
-
-from deform import ValidationFailure
 
 from phoenix.utils import wps_describe_url
 
 from phoenix.processes.views.execute import ExecuteProcess
+from phoenix.wps import appstruct_to_inputs
 
 import logging
 logger = logging.getLogger(__name__)
@@ -16,7 +14,12 @@ logger = logging.getLogger(__name__)
 @view_defaults(permission='view', layout='default')
 class ExecuteProcessJson(ExecuteProcess):
     def __init__(self, request):
-        ExecuteProcess.__init__(self, request)
+        self.exception = None
+        try:
+            ExecuteProcess.__init__(self, request)
+        except Exception as e:
+            #Cannot initialized the process specified in the request
+            self.exception = e
 
     def jsonify(self, value):
         # ComplexData type
@@ -26,17 +29,39 @@ class ExecuteProcessJson(ExecuteProcess):
         else:
             return value
 
-    def process_form(self, form):
-        controls = self.request.POST.items()
+    @view_config(route_name='processes_execute', request_method='POST', renderer='json', accept='application/json')
+    def execute(self):
+        inputs = []
+        async = True
         try:
-            # TODO: uploader puts qqfile in controls
-            controls = [control for control in controls if 'qqfile' not in control[0]]
-            logger.debug("before validate %s", controls)
-            appstruct = form.validate(controls)
-            logger.debug("before execute %s", appstruct)
-            task_id = self.execute(appstruct)
+            if self.exception:
+                raise self.exception
+
+            if 'submit' in self.request.POST:
+                form = self.generate_form()
+                controls = self.request.POST.items()
+                controls = [control for control in controls if 'qqfile' not in control[0]]
+                logger.debug("before validate %s", controls)
+                try:
+                    appstruct = form.validate(controls)
+                except Exception as e:
+                    raise Exception(str(form.error))
+
+                logger.debug("before execute %s", appstruct)
+                inputs = appstruct_to_inputs(self.request, appstruct)
+                async = appstruct.get('_async_check', True)
+            elif self.request.content_type == 'application/json':
+                for key, values in self.request.json_body.items():
+                    if not isinstance(values, types.ListType):
+                        values = [values]
+                    for value in values:
+                        inputs.append((key, value))
+            else:
+                raise Exception('Request content type must be form-data or application/json')
+
+            task_id = ExecuteProcess.execute(self, inputs=inputs, async=async)
         except Exception as e:
-            logger.exception('validation of exectue view failed.')
+            logger.exception('validation of execute view failed.')
             return dict(
                 status=520,
                 message=str(e),
@@ -47,26 +72,6 @@ class ExecuteProcessJson(ExecuteProcess):
             message='OK',
             task_id=task_id
         )
-
-    @view_config(route_name='processes_execute', request_method='POST', renderer='json', accept='application/json')
-    def execute(self):
-        if 'submit' in self.request.POST:
-            form = self.generate_form()
-            return self.process_form(form)
-        elif self.request.content_type == 'application/json':
-            inputs = []
-            for key, values in self.request.json_body.items():
-                if not isinstance(values, types.ListType):
-                    values = [values]
-                for value in values:
-                    inputs.append((key, value))
-        else:
-            raise HTTPBadRequest(detail='Request content type must be application/json')
-
-        try:
-            return dict(task_id=ExecuteProcess.execute(self, inputs=inputs))
-        except Exception as e:
-            raise HTTPBadRequest(detail=e.message)
 
     @view_config(route_name='processes_execute', request_method='GET', renderer='json', accept='application/json')
     def get_capabilities(self):
